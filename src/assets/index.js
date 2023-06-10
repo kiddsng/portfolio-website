dfaf71e0e0970795315e29c003373de23ab252f2
Kidd Sng <sngkidd@outlook.com>
2023-06-10 13:55:05 +0800

    Add temasekpolytechnic.jpg to assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,7 @@
 import avatar from "./icons/avatar.png";
 
 import astar from "./companies/astar.png";
+import temasekpolytechnic from "./companies/temasekpolytechnic.jpg";
 
 import polyfintech from "./competitions/polyfintech.png";
 import sussanl from "./competitions/sussanl.png";
@@ -14,6 +15,7 @@ import reactjs from "./technologies/reactjs.png";
 export {
 	avatar,
 	astar,
+	temasekpolytechnic,
 	polyfintech,
 	sussanl,
 	css,
