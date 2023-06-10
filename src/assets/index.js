1899a038ce70e527a1d3258e8d18161bb92e0338
Kidd Sng <sngkidd@outlook.com>
2023-06-10 12:57:47 +0800

    Update index.js in assets
    Remove unneccessary images due to switch to Font Awesome icons, and update index.js based on the current images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,44 +1,24 @@
-import arrowdown from "./icons/arrowdown.png";
 import avatar from "./icons/avatar.png";
-import bronze from "./icons/bronze.png";
-import close from "./icons/close.svg";
-import email from "./icons/email.png";
-import github from "./icons/github.png";
-import gold from "./icons/gold.png";
-import link from "./icons/link.png";
-import linkedin from "./icons/linkedin.png";
-import logo from "./icons/logo.png";
-import menu from "./icons/menu.svg";
-import silver from "./icons/silver.png";
-import user from "./icons/user.png";
 
 import astar from "./companies/astar.png";
 
 import polyfintech from "./competitions/polyfintech.png";
 import sussanl from "./competitions/sussanl.png";
 
-import carrent from "./projects/carrent.png";
-import jobit from "./projects/jobit.png";
-import tripguide from "./projects/tripguide.png";
+import css from "./technologies/css.png";
+import html from "./technologies/html.png";
+import javascript from "./technologies/javascript.png";
+import python from "./technologies/python.svg";
+import reactjs from "./technologies/reactjs.png";
 
 export {
-    arrowdown,
-    avatar,
-    bronze,
-    close,
-    email,
-    github,
-    gold,
-    link,
-    linkedin,
-    logo,
-    menu,
-    silver,
-    user,
-    astar,
-    polyfintech,
-    sussanl,
-    carrent,
-    jobit,
-    tripguide,
+	avatar,
+	astar,
+	polyfintech,
+	sussanl,
+	css,
+	html,
+	javascript,
+	python,
+	reactjs,
 };
