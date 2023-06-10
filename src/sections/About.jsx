ddce6f36b67ee0172648b795f23b0706a98b239c
Kidd Sng <sngkidd@outlook.com>
2023-06-10 12:59:36 +0800

    Update About section
    Refine designs and add technologies div

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
+import { technologies } from "../constants";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { textVariant, fadeIn } from "../utils/motion";
@@ -10,23 +11,41 @@ const About = () => {
 	return (
 		<>
 			<motion.div variants={textVariant()}>
-				<p className={`${styles.sectionSubText}`}>Introduction</p>
-				<h2 className={`${styles.sectionHeadText}`}>Overview.</h2>
+				<h2 className={`${styles.sectionHeadText}`}>About</h2>
+				<p className={`${styles.sectionSubText}`}>My introduction</p>
 			</motion.div>
-			<motion.p
-				variants={fadeIn("", "", 0.1, 1)}
-				className="mt-4 text-[18px] text-black-100 dark:text-white sm:text-[24px]"
-			>
-				Hi! Welcome to my website, my name is Kidd. I recently graduated from
-				Temasek Polytechic with a Diploma in Big Data & Analytics with Merit. I
-				am interested in coding and programming in general. Currently, I am
-				mainly using Python, however, I have some experience in other languages
-				like HTML, CSS and JS in web development, and SQL and R for data
-				analytics. This GitHub account is for educational purposes and to show
-				some of the projects that I have done. I am always happy to learn, so
-				feel free to drop an email or message for me if you see or have any
-				problem, issue or advice about the stuff I do here! :)
-			</motion.p>
+			<motion.div variants={fadeIn("", "", 0.1, 1)} className="mt-20">
+				<p className="mt-20 text-[18px] text-[#252525] dark:text-[#f5f5f5] sm:text-[24px]">
+					My name is Kidd Sng. I recently graduated from Temasek Polytechic with
+					a Diploma in Big Data & Analytics with Merit. I am an avid coder, and
+					I like to work on my personal projects when I have some free time. I
+					am interested in programming in general, and so far I have already
+					work with a few modern languages.
+					<br />
+					<br />
+					Currently, I have some experience in languages such as HTML, CSS and
+					JS in web development, and Python and SQL for data analytics.
+					<br />
+					<br />
+					Most of the projects that I've done are either for educational
+					purposes or personal interests. I have showcased some of my projects
+					here and on my GitHub account. Feel free to check them out! Please
+					drop me an email or a message if you see or have any problem, issue,
+					or advice.{" "}
+					<span className="fa-solid fa-face-smile text-yellow-500"></span>
+				</p>
+				<div className="mt-12 flex flex-row justify-center gap-12">
+					{technologies.map((technology) => (
+						<div className="flex h-20 w-20 items-center justify-center rounded-full bg-white dark:bg-[#303741]">
+							<img
+								src={technology.icon}
+								alt={technology.name}
+								className="h-12 w-12 object-contain"
+							/>
+						</div>
+					))}
+				</div>
+			</motion.div>
 		</>
 	);
 };
